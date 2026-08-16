720c6ef6de9a43a67c144f56a99d4556b27f3256
rkurapati42-sys <rkurapati42@gmail.com>
2026-08-16 12:26:26 +0530

    Update V2.0__expand_add_encrypted_email.sql

--- pipeline/database/migrations/V2.0__expand_add_encrypted_email.sql
+++ pipeline/database/migrations/V2.0__expand_add_encrypted_email.sql
@@ -1,1 +1,7 @@
 
+-- EXPAND PHASE: Zero-downtime backward-compatible column expansion
+ALTER TABLE customer_profiles ADD COLUMN encrypted_email BYTEA;
+CREATE INDEX CONCURRENTLY idx_customer_encrypted_email ON customer_profiles (encrypted_email);
+
+INSERT INTO schema_audit_log (migration_id, description, executed_by, phase)
+VALUES ('V2.0', 'Expand: Add encrypted_email', current_user, 'EXPAND');
